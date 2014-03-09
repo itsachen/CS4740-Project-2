61c1022a24fd70584cf758f509f07010e9034ca6
moey509 <mso32@cornell.edu>
2014-03-09 03:45:21 -0400

    fixed parser

--- Parser.py
+++ Parser.py
@@ -19,10 +19,10 @@ class Word:
 
     def toString(self):
         s = self.word + " : " + self.pos + "\n"
-        counter = 1
-        while counter in sense_id_map:
-            for context in sense_id_map[counter]:
-                s += "s"
+        for counter, context_list in self.sense_id_map.items():
+            for context in context_list:
+                s += str(counter) + " " + context.toString() + "\n"
+        return s        
 
 class Context:
     def __init__(self, context):
@@ -37,9 +37,11 @@ class Context:
         self.after_context = nltk.word_tokenize(after_sentences1.pop(0))
 
         for sentence in prev_sentences1:
-            self.prev_sentences.append(nltk.word_tokenize(sentence))
+            for wd in nltk.word_tokenize(sentence):
+                self.prev_sentences.append(wd)
         for sentence in after_sentences1:
-            self.after_sentences.append(nltk.word_tokenize(sentence))
+            for wd in nltk.word_tokenize(sentence):
+                self.after_sentences.append(wd)
 
     def toString(self):
         s = ""
@@ -48,7 +50,7 @@ class Context:
         s += " ##" 
         for token in self.prev_context:
             s += " " + token
-        s += " ###" 
+        s += " ####" 
         for token in self.after_context:
             s += " " + token
         s += " ##" 
@@ -60,21 +62,25 @@ class Context:
 def parse_test_data(filename):
     word_map = {}
     with open(filename) as f:
+        linecounter = 0
         for line in f:
-            parts = re.split(' \| ', line)
-            word = re.split('\.', parts[0])[0]
-            pos = re.split('\.', parts[0])[1]
-            sense = parts[1]
-            context = parts[2]
+            if linecounter < 50:
+                parts = re.split(' \| ', line)
+                word = re.split('\.', parts[0])[0]
+                pos = re.split('\.', parts[0])[1]
+                sense = parts[1]
+                context = parts[2]
 
-            #print word + " # " + pos + " # " + sense + " # " + context
+                #print word + " # " + pos + " # " + sense + " # " + context
 
-            if not word in word_map:
-                word_map[word] = Word(word, pos)
-            word_map[word].add_context(sense, context)
+                if not word in word_map:
+                    word_map[word] = Word(word, pos)
+                word_map[word].add_context(sense, context)
+                linecounter += 1
 
     return word_map     
 
-
-for x in parse_test_data(train_file):
-    print      
+map1 = parse_test_data(train_file)
+print len(map1)
+for x in map1:
+    print map1[x].toString()
