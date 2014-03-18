02c8709433fd29fdacdb27315b7c31c23482a941
moey509 <mso32@cornell.edu>
2014-03-17 23:55:25 -0400

    actually convert tabs to spaces

--- contextBasedWSD.py
+++ contextBasedWSD.py
@@ -17,78 +17,78 @@ bigram_weight = 1.0
 
 # from a word_map creates a map of words to senses
 def get_ngram_map(word_map, n):
-	word_to_sense_to_ngram_map = {}
-	for word_object in word_map.values():
-		sense_to_ngram_map = {}
-		for sense, context_list in word_object.sense_id_map.items():
-			sense_to_ngram_map[sense] = create_ngram_from_context_list(word_object.word,context_list,n)
-		word_to_sense_to_ngram_map[word_object.word] = sense_to_ngram_map
-	return word_to_sense_to_ngram_map
+    word_to_sense_to_ngram_map = {}
+    for word_object in word_map.values():
+        sense_to_ngram_map = {}
+        for sense, context_list in word_object.sense_id_map.items():
+            sense_to_ngram_map[sense] = create_ngram_from_context_list(word_object.word,context_list,n)
+        word_to_sense_to_ngram_map[word_object.word] = sense_to_ngram_map
+    return word_to_sense_to_ngram_map
 
 def create_ngram_from_context_list(word, context_list, n):
-	tokenized_context_list = []
-	for context in context_list:
-		tokenized_context = ["<s>"]
-		tokenized_context.extend(context.prev_context)
-		tokenized_context.append(word)
-		tokenized_context.extend(context.after_context)
-		tokenized_context.append("<e>")
-		tokenized_context_list.append(tokenized_context)
-	probabilityTable, prob, wordSet = ngram.create_smoothed_ngram_probability_table(tokenized_context_list,n)
-	return probabilityTable
+    tokenized_context_list = []
+    for context in context_list:
+        tokenized_context = ["<s>"]
+        tokenized_context.extend(context.prev_context)
+        tokenized_context.append(word)
+        tokenized_context.extend(context.after_context)
+        tokenized_context.append("<e>")
+        tokenized_context_list.append(tokenized_context)
+    probabilityTable, prob, wordSet = ngram.create_smoothed_ngram_probability_table(tokenized_context_list,n)
+    return probabilityTable
 
 def get_ngram_probability(word, context, ngram_map, n):
-	#tokenized_context = ["<s>"]
-	tokenized_context = []
-	tokenized_context.extend(context.prev_context)
-	tokenized_context.append(word)
-	tokenized_context.extend(context.after_context)
-	#tokenized_context.append("<e>")
+    #tokenized_context = ["<s>"]
+    tokenized_context = []
+    tokenized_context.extend(context.prev_context)
+    tokenized_context.append(word)
+    tokenized_context.extend(context.after_context)
+    #tokenized_context.append("<e>")
 
-	word_ngram = ngram_map[word]
+    word_ngram = ngram_map[word]
 
-	accumulated_probability = {}
-	for sense in word_ngram.keys():
-		accumulated_probability[sense] = 1.0
-	if n == 1:
-		for sense, ngram in word_ngram.items():
-			for token in tokenized_context:
-				if token in ngram:
-					accumulated_probability[sense] *= ngram[token]
-	else:
-		for sense, ngram in word_ngram.items():
-			prev_token = None;
-			for token in tokenized_context:
-				if prev_token == None:
-					prev_token = token
-				else:
-					if prev_token in ngram and token in ngram[prev_token]:
-						accumulated_probability[sense] *= ngram[prev_token][token] 
-						prev_token = token
-	return accumulated_probability
+    accumulated_probability = {}
+    for sense in word_ngram.keys():
+        accumulated_probability[sense] = 1.0
+    if n == 1:
+        for sense, ngram in word_ngram.items():
+            for token in tokenized_context:
+                if token in ngram:
+                    accumulated_probability[sense] *= ngram[token]
+    else:
+        for sense, ngram in word_ngram.items():
+            prev_token = None;
+            for token in tokenized_context:
+                if prev_token == None:
+                    prev_token = token
+                else:
+                    if prev_token in ngram and token in ngram[prev_token]:
+                        accumulated_probability[sense] *= ngram[prev_token][token] 
+                        prev_token = token
+    return accumulated_probability
 
 def word_sense_disambiguation(unclassified_words, word_map):
-	output = []
+    output = []
 
-	unagram = get_ngram_map(word_map, 1)
-	bigram = get_ngram_map(word_map, 2)
-	for word in unclassified_words:
-		sense_scores = {}
-		context = word.sense_id_map[0][0]
-		for sense, score in get_ngram_probability(word.word, context, unagram, 1).items():
-			sense_scores[sense] = score * unagram_weight
-		for sense, score in get_ngram_probability(word.word, context, bigram, 2).items():
-			sense_scores[sense] += score * bigram_weight
+    unagram = get_ngram_map(word_map, 1)
+    bigram = get_ngram_map(word_map, 2)
+    for word in unclassified_words:
+        sense_scores = {}
+        context = word.sense_id_map[0][0]
+        for sense, score in get_ngram_probability(word.word, context, unagram, 1).items():
+            sense_scores[sense] = score * unagram_weight
+        for sense, score in get_ngram_probability(word.word, context, bigram, 2).items():
+            sense_scores[sense] += score * bigram_weight
 
-		#determine output
-		current_max = 0.0
-		current_sense = 0
-		for sense, score in sense_scores.items():
-			if score >= current_max:
-				current_sense = sense
-				current_max = score
-		output.append(current_sense)
-	return output
+        #determine output
+        current_max = 0.0
+        current_sense = 0
+        for sense, score in sense_scores.items():
+            if score >= current_max:
+                current_sense = sense
+                current_max = score
+        output.append(current_sense)
+    return output
 
 def write_to_file(filename, prediction_list):
     file_object = open(filename, 'w+')    
